06894df72099db54d4961b104d948e2e8c49a50d
Adrian <alextravc.342@gmail.com>
2023-05-08 19:20:40 -0500

    [ADD]: initial skeleton files for theme

---
+++ z_moodle-yt/a_moodle-tips/c_theme/lang/en/theme_adrian.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Photo backgrounds.
+ *
+ * @package    theme_adrian
+ * @copyright  2023 Adrian
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+// This line protects the file from being accessed by a URL directly.
+defined('MOODLE_INTERNAL') || die();
+
+// A description shown in the admin theme selector.
+$string['choosereadme'] = 'Theme Adrian is a child theme of Boost. It adds the ability to upload background Adrians.';
+
+// The name of our plugin.
+$string['pluginname'] = 'Adrian';
+
+// We need to include a lang string for each block region.
+$string['region-side-pre'] = 'Right';
---
+++ z_moodle-yt/a_moodle-tips/c_theme/notes.md
@@ -0,0 +1,33 @@
+# Moodle theme tutoria
+
+## Create a child theme based off boost
+- --- Vamos a crear 1 child theme
+	- A la altura de    /boost   creamos nuestro child theme
+	
+
+	- --- Creamos el    `/adrian`   dir de nuestro theme
+  	- Creamos el    version.php,   /lang,    /pix
+    	- /pix contendra el favicon y demas necesario para el theme, copie del  boot
+  	- Creamos el    lib.php
+  	- Creamos el    config.php
+    	- Es uno de los mas importantes, con este file ya podmos probarlo
+    	- Aqui es Importante especificar el      parent theme     del q extiende
+
+
+	- --- Como 1 theme es otro tipo de plugin, simplemente se instala como siempre
+  	- Instalamos el theme
+  	- Cambiamos el theme para usar el nuestro:
+    	- Site Admin > Apparence > Theme > Theme selector
+  	- Con esto ya estamos usando nuesto theme
+
+		- -- Asi hemos creamos el skeleton de nuestro child theme
+  		- 
+
+
+
+
+		-- URL:
+			- Creating a theme based on boost:		https://docs.moodle.org/dev/Creating_a_theme_based_on_boost
+			- 
+
+
